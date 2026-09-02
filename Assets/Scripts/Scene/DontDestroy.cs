c28ab20e0a97bf76458dc7f5fed2404077ab7807
KuroShiro <fvch199594@gmail.com>
2026-09-02 15:12:12 -0500

    Actualizacion 8.2
    Correciones Pausa y DontDestroyd

--- Assets/Scripts/Scene/DontDestroy.cs
+++ Assets/Scripts/Scene/DontDestroy.cs
@@ -1,20 +1,47 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class DontDestroy : MonoBehaviour
 {
     private static GameObject[] persistentObject = new GameObject[10];
-    public int objectIndex;
-    void Awake()
-    {
-        if(persistentObject[objectIndex] == null)
-        {
 
+    public int objectIndex;
+
+    private void Awake()
+    {
+        if (persistentObject[objectIndex] == null)
+        {
             persistentObject[objectIndex] = gameObject;
             DontDestroyOnLoad(gameObject);
         }
-        else if(persistentObject[objectIndex] != gameObject)
+        else if (persistentObject[objectIndex] != gameObject)
         {
             Destroy(gameObject);
+        }
+    }
+
+    private void OnEnable()
+    {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    private void OnDisable()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        if (scene.name == "INTRO")
+        {
+            for (int i = 0; i < persistentObject.Length; i++)
+            {
+                if (persistentObject[i] != null)
+                {
+                    Destroy(persistentObject[i]);
+                    persistentObject[i] = null;
+                }
+            }
         }
     }
 }
